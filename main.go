8558e4371d7004875202f91dcd6f8f181d428298
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-11-13 14:52:56 -0300

    feat: add main logic for CLI todo app

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"todo-cli-go/cli"
+	"todo-cli-go/storage"
+	"todo-cli-go/todo"
+)
 
+func main() {
+	todos := todo.Todos{}
+	storage := storage.NewStorage[todo.Todos]("todos.json")
+	defer storage.Close()
+
+	storage.Load(&todos)
+	cmdFlags := cli.NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.Save(todos)
 }
